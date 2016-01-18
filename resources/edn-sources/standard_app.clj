4904a0b4426e21196ce811829cc9a54b582d8847
Novak Boskov <gnovak.boskov@gmail.com>
2016-01-18 16:24:06 +0100

    Cardinality relationship gui representation and :meta section

--- resources/edn-sources/standard_app.clj
+++ resources/edn-sources/standard_app.clj
@@ -2,16 +2,20 @@
  {:name "Pilot model"
   :author "Novak Boskov"
   :comment "This is just in the sake of a proof of concept."}
+ :meta
+ {:api-only false}
  :entities
  [{:signature "Musician"
    :properties [{:name "name" :type :str :gui-label "Name"}
                 {:name "surname" :type :str :gui-label "Surname"}
                 {:name "nickname" :type :str :gui-label "nick"}
                 {:name "honors" :type {:coll :str}}
-                {:name "band" :type {:one :ref-to :signature "Band"}
+                {:name "band" :type {:one :ref-to
+                                     :signature "Band"
+                                     :gui :select-form}
                  :gui-label "Of band"}
-                {:name "social-profile" :type {:one :ref-to :signature
-                                               "SocialMediaProfile"}
+                {:name "social-profile" :type {:one :ref-to
+                                               :signature "SocialMediaProfile"}
                  :gui-label "profile"}]}
   {:signature "Band"
    :properties [{:name "name" :type :str :gui-label "Name"}
@@ -19,23 +23,33 @@
                 {:name "largeness" :type :str :gui-label "Largeness"}
                 {:name "members" :type {:coll :ref-to :signature "Musician"}
                  :gui-label "Members"}
-                {:name "category" :type {:one :ref-to :signature "Category"}
+                {:name "category" :type {:one :ref-to
+                                         :signature "Category"
+                                         :gui :drop-list}
                  :gui-label "Category"}
-                {:name "participated" :type {:coll :ref-to :signature "Festival"}
+                {:name "participated" :type {:coll :ref-to
+                                             :signature "Festival"
+                                             :gui :table-view}
                  :gui-label "Participated in"}]}
   {:signature "Category"
    :properties [{:name "name" :type :str :gui-label "Name"}
                 {:name "subcategories" :type {:coll :ref-to :signature "Category"}
                  :gui-label "subcategories"}
-                {:name "bands" :type {:coll :ref-to :signature "Band"}
+                {:name "bands" :type {:coll :ref-to
+                                      :signature "Band"
+                                      :gui :table-view}
                  :gui-label "Bands of category"}]}
   {:signature "Festival"
    :properties [{:name "name" :type :str :gui-label "Name"}
                 {:name "location" :type :geo :gui-label "Loco"}
-                {:name "bands" :type {:coll :ref-to :signature "Band"}
+                {:name "bands" :type {:coll :ref-to
+                                      :signature "Band"
+                                      :gui :table-view}
                  :gui-label "participant bands"}]}
   {:signature "SocialMediaProfile"
-   :properties [{:name "owner" :type {:one :ref-to :signature "Musician"}
+   :properties [{:name "owner" :type {:one :ref-to
+                                      :signature "Musician"
+                                      :gui :select-form}
                  :gui-label "Name"}
                 {:name "name" :type :str :gui-label "name"}
                 {:name "provider" :type :str :gui-label "provider"}]}]}
