464e76529a5a56dc4750cb4b16ad2cd1b296c19b
Novak Boskov <gnovak.boskov@gmail.com>
2016-01-25 10:36:37 +0100

    Fixing tests
    to-graph function is renamed according to Clojure style guide.
    See https://github.com/bbatsov/clojure-style-guide#naming

--- test/larva/graph_test.clj
+++ test/larva/graph_test.clj
@@ -7,50 +7,50 @@
 
 (deftest root-successors-test
   (testing "Number of root node successors."
-    (is (= 2 (-> (lg/to-graph standard-program-1)
+    (is (= 2 (-> (lg/->graph standard-program-1)
                  (g/successors lg/root-node)
                  count)))
-    (is (= 1 (-> (lg/to-graph standard-program-2)
+    (is (= 1 (-> (lg/->graph standard-program-2)
                  (g/successors lg/root-node)
                  count)))
-    (is (= 3 (-> (lg/to-graph standard-program-with-meta)
+    (is (= 3 (-> (lg/->graph standard-program-with-meta)
                  (g/successors lg/root-node)
                  count)))))
 
 (deftest root-successors-names-test
   (testing "Root node successors names."
-    (is (= #{"about" "meta" :entities} (-> (lg/to-graph standard-program-with-meta)
+    (is (= #{"about" "meta" :entities} (-> (lg/->graph standard-program-with-meta)
                                            (g/successors lg/root-node)
                                            set)))))
 
 (deftest entities-successors-test
   (testing "Number of entities-dispatch-node successors."
-    (is (= 3 (-> (lg/to-graph standard-program-1)
+    (is (= 3 (-> (lg/->graph standard-program-1)
                  (g/successors lg/entities-node)
                  count)))
-    (is (= 4 (-> (lg/to-graph standard-program-11)
+    (is (= 4 (-> (lg/->graph standard-program-11)
                  (g/successors lg/entities-node)
                  count)))
-    (is (= 3 (-> (lg/to-graph standard-program-2)
+    (is (= 3 (-> (lg/->graph standard-program-2)
                  (g/successors lg/entities-node)
                  count)))
-    (is (= 0 (-> (lg/to-graph no-entities-edge-case)
+    (is (= 0 (-> (lg/->graph no-entities-edge-case)
                  (g/successors lg/entities-node)
                  count)))
-    (is (= 0 (-> (lg/to-graph no-entities-no-about-edge-case)
+    (is (= 0 (-> (lg/->graph no-entities-no-about-edge-case)
                  (g/successors lg/entities-node)
                  count)))))
 
 (deftest entity-property-test
   (testing "Name of properties of a specific entity."
     (is (= #{"name" "surname" "nickname" "honors"}
-           (let [g (lg/to-graph standard-program-1)
+           (let [g (lg/->graph standard-program-1)
                  n "Musician"]
              (->> (g/successors g n)
                   (mapv #(:name (g/attrs g %)))
                   set))))
     (is (= #{"name" "location"}
-           (let [g (lg/to-graph standard-program-1)
+           (let [g (lg/->graph standard-program-1)
                  n "Festival"]
              (->> (g/successors g n)
                   (mapv #(:name (g/attrs g %)))
@@ -68,30 +68,30 @@ relationships with their cardinality."
           p3 {:name "bands" :type {:coll :ref-to :signature "Band"}
               :gui-label "participant bands"}]
       (is (= 0
-             (-> (lg/to-graph standard-program-no-refs)
+             (-> (lg/->graph standard-program-no-refs)
                  (g/out-edges (lg/build-property-label p0 "Musician"))
                  count)))
       (is (= 0
-             (-> (lg/to-graph standard-program-no-refs)
+             (-> (lg/->graph standard-program-no-refs)
                  (g/out-edges (lg/build-property-label p01 "Musician"))
                  count)))
       ;; whole-part relationship
       (is (= "Category"
-             (-> (lg/to-graph standard-program-cardinality-1)
+             (-> (lg/->graph standard-program-cardinality-1)
                  (g/out-edges (lg/build-property-label p1 "Category"))
                  first :dest)))
       (is (= "Band"
-             (-> (lg/to-graph references-1)
+             (-> (lg/->graph references-1)
                  (g/out-edges (lg/build-property-label p2 "Musician"))
                  first :dest)))
       (is (= "Band"
-             (-> (lg/to-graph references-1)
+             (-> (lg/->graph references-1)
                  (g/out-edges (lg/build-property-label p3 "Festival"))
                  first :dest))))))
 
 (deftest property-reference-cardinality-test
   (testing "Cardinality of the property-entity relationships."
-    (let [g (lg/to-graph references-1)
+    (let [g (lg/->graph references-1)
           p1 {:name "band" :type {:one :ref-to :signature "Band"}
               :gui-label "Of band"}
           p2 {:name "bands" :type {:coll :ref-to :signature "Band"}
@@ -120,7 +120,7 @@ relationships with their cardinality."
 (deftest cardinality-graph-presentation-test
   (testing "If cardinality expressed through uni-model is established in graph
  representation of the program."
-    (let [g (lg/to-graph references-1)
+    (let [g (lg/->graph references-1)
           band-particip {:name "participated" :type {:coll :ref-to :signature
                                                      "Festival"}
                          :gui-label "Participated in"}
